f5bd390413b9b3e957258869def3f4c41a0c19f9
Claas Ahlrichs <claasahl@gmail.com>
2021-11-06 11:15:44 +0000

    simplified sample

--- main.ts
+++ main.ts
@@ -1,57 +1,45 @@
 import { ethers } from "ethers";
 
 type State = {
-    fromContract: number | undefined,
-    fromQueryFilter: number | undefined,
-    received: number
+    logsFromPoller1: number | undefined,
+    logsFromPoller2: number | undefined,
 }
-const state = new Map<string, State>();
+const state = new Map<number, State>();
 const label = "SYNC-EVENT";
 
-function onSyncEventFromContract(reserve0: ethers.BigNumber, reserve1: ethers.BigNumber, event: ethers.Event): void {
-    const key = `${reserve0},${reserve1}`;
-    console.log("fromContract   ", key, event.blockNumber);
-    const value = state.get(key);
-    state.set(key, {
-        fromContract: event.blockNumber, // <-- mark event as "received via contract.on(...)"
-        fromQueryFilter: value?.fromQueryFilter,
-        received: Date.now()
-    })
+function isMisaligned(blockNumber: number): boolean {
+    const s = state.get(blockNumber);
+    return !!s && // <-- do we have state information at all?
+        typeof s.logsFromPoller1 === "number" && // <-- let's make sure that both pollers completed their work
+        typeof s.logsFromPoller2 === "number" &&
+        s.logsFromPoller1 !== s.logsFromPoller2; // <-- did pollers yield a different number of logs?
 }
 
-function onSyncEventsFromQueryFilter(events: ethers.Event[]): void {
-    for (const event of events) {
-        const key = `${event.args?.reserve0},${event.args?.reserve1}`;
-        console.log("fromQueryFilter", key, event.blockNumber);
-        const value = state.get(key);
-        state.set(key, {
-            fromContract: value?.fromContract,
-            fromQueryFilter: event.blockNumber, // <-- mark event as "received via contact.queryFilter(...)"
-            received: Date.now()
-        })
+function lookForMisalignedState(blockNumber: number) {
+    if (isMisaligned(blockNumber)) {
+        console.timeLog(label, "found misaligned state", blockNumber, state.get(blockNumber));
+        process.exit(1);
     }
 }
 
-function onErrorFromContract(error: Error, event: ethers.Event): void {
-    console.log("fromContract >> error:", error, event)
+function updateState(blockNumber: number, value: number, key: keyof State): void {
+    const s = state.get(blockNumber);
+    state.set(blockNumber, {
+        logsFromPoller1: undefined,
+        logsFromPoller2: undefined,
+        ...(s ?? {}),
+        [key]: value
+    });
 }
 
-function lookForMissingEventsFromContract(callback: (state: State) => void) {
-    const halfMinute = 30000;
-    for (const [key, value] of state.entries()) {
-        const now = Date.now();
-        if (now - halfMinute < value.received) {
-            // let's wait for the timeout
-        } else if (value.fromContract && value.fromQueryFilter) {
-            // event was received via contract.on(...) and contract.queryFilter(...)
-            // i.e. everything is just fine :)
-            state.delete(key); // <-- trying to keep the state/cache as small as possible
-            continue;
-        } else {
-            console.timeLog(label, "found missing event", key, value);
-            callback(value);
-        }
-    }
+async function poll(provider: ethers.providers.Provider, filter: ethers.EventFilter, blockNumber: number, key: keyof State) {
+    const logs = await provider.getLogs({
+        ...filter,
+        fromBlock: blockNumber,
+        toBlock: blockNumber
+    });
+    updateState(blockNumber, logs.length, key);
+    lookForMisalignedState(blockNumber);
 }
 
 function main() {
@@ -62,39 +50,12 @@ function main() {
     const contract = new ethers.Contract("0x8fa59693458289914db0097f5f366d771b7a7c3f", [
         "event Sync(uint112 reserve0, uint112 reserve1)"
     ], provider)
-
-    // eventually, this will skip "Sync" events
-    contract.on("Sync", onSyncEventFromContract);
-    contract.on("error", onErrorFromContract);
-
-    // poll "Sync" events for reference
+    
     const filter = contract.filters["Sync"]();
-    const onMissingEvent = (value: State) => {
-        const blockNumber = value.fromContract || value.fromQueryFilter || 0;
-        provider.getLogs({
-            ...filter,
-            fromBlock: blockNumber,
-            toBlock: blockNumber
-        }).then(logs => {
-            const abi = new ethers.utils.AbiCoder();
-            logs.forEach(log => {
-                const [reserve0, reserve1] = abi.decode(["uint112", "uint112"], log.data);
-                const key = `${reserve0},${reserve1}`;
-                console.log("fromGetLogs", key, log.blockNumber);
-            });
-            process.exit(1);
-        }).catch(err => {
-            console.log(">>> err", err);
-            process.exit(1);
-        })
-    }
     provider.on("block", blockNumber => {
         console.log("new block", blockNumber);
-        lookForMissingEventsFromContract(onMissingEvent); // <-- use "block"-events as the driving force to (re-)check for missing events
-        setImmediate(async () => {
-            const events = await contract.queryFilter(filter, blockNumber);
-            onSyncEventsFromQueryFilter(events);
-        })
-    })
+        setImmediate(() => poll(provider, filter, blockNumber, "logsFromPoller1"));
+        setImmediate(() => poll(provider, filter, blockNumber, "logsFromPoller2"));
+    });
 }
 main();
--- scripts/wait-for-missing-event.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-ACTION="RESTART";
-while [ "${ACTION}" != 'STOP' ]; do
-    ACTION="STOP";
-    npm start || ACTION="RESTART";
-done
